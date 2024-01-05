5a0faa0d2bca54c9b608bf0e5805cada04868fb5
Ronan Kelley <44382880+Ronan-Kelley@users.noreply.github.com>
2024-01-05 13:51:46 -0500

    early drive objects

---
+++ RI3D/src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drive extends SubsystemBase {
+    public Drive() {
+
+    }
+
+    @Override
+    public void periodic() {}
+}
